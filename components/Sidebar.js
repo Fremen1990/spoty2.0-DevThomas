075446a0d0d15e484e41c6c92be8a85994f3c444
Fremen1990 <fremen1990@gmail.com>
2022-01-17 15:32:43 +0100

    first commit

---
+++ components/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sidebar = () => {
+    return (
+        <div>
+            <h1>I am a SIDE BAR!</h1>
+        </div>
+    );
+};
+
+export default Sidebar;
---
+++ pages/index.tsx
@@ -0,0 +1,23 @@
+import Sidebar from "../components/Sidebar"
+
+export default function Home() {
+    return (
+        <div className="">
+
+            <h1>This is COOL Spotiyt THOMAS build!</h1>
+
+
+            <main>
+
+
+                <Sidebar/>
+                {/*<Center></Center>*/}
+            </main>
+
+            <div>
+                {/*Player*/}
+            </div>
+
+        </div>
+    )
+}
